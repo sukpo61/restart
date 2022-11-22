a9ab30d1ea1066b25b519b55579f9d8089ea8a31
GH <sukpo6010@gmail.com>
2022-11-22 22:55:51 +0900

    프로필기능적용

--- js/pages/profile.js
+++ js/pages/profile.js
@@ -9,6 +9,8 @@ import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 
 export const changeProfile = async (event) => {
   event.preventDefault();
+  console.log("함수 호출됨");
+
   document.getElementById("profileBtn").disabled = true;
   const imgRef = ref(
     storageService,
@@ -17,11 +19,12 @@ export const changeProfile = async (event) => {
 
   const newNickname = document.getElementById("profileNickname").value;
   // 프로필 이미지 dataUrl을 Storage에 업로드 후 다운로드 링크를 받아서 photoURL에 저장.
-  const imgDataUrl = localStorage.getItem("imgDataUrl");
+  const imgDataUrl2 = localStorage.getItem("imgDataUrl2");
   let downloadUrl;
-  if (imgDataUrl) {
-    const response = await uploadString(imgRef, imgDataUrl, "data_url");
+  if (imgDataUrl2) {
+    const response = await uploadString(imgRef, imgDataUrl2, "data_url");
     downloadUrl = await getDownloadURL(response.ref);
+    console.log("downloadurl: ", downloadUrl);
   }
   await updateProfile(authService.currentUser, {
     displayName: newNickname ? newNickname : null,
@@ -29,7 +32,8 @@ export const changeProfile = async (event) => {
   })
     .then(() => {
       alert("프로필 수정 완료");
-      window.location.hash = "#fanLog";
+      window.location.hash = "#mypage";
+      window.location.reload()
     })
     .catch((error) => {
       alert("프로필 수정 실패");
@@ -43,8 +47,9 @@ export const onFileChange = (event) => {
   reader.readAsDataURL(theFile); // file 객체를 브라우저가 읽을 수 있는 data URL로 읽음.
   reader.onloadend = (finishedEvent) => {
     // 파일리더가 파일객체를 data URL로 변환 작업을 끝났을 때
-    const imgDataUrl = finishedEvent.currentTarget.result;
-    localStorage.setItem("imgDataUrl", imgDataUrl);
-    document.getElementById("profileView").src = imgDataUrl;
+    const imgDataUrl2 = finishedEvent.currentTarget.result;
+    localStorage.setItem("imgDataUrl2", imgDataUrl2);
+    document.getElementById("mypage_profile").src = imgDataUrl2;
+
   };
 };
